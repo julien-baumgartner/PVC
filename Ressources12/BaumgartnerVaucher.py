ed04730bf4acf1e49b3d391bf9a5dc3d72d0c3a4
julien-baumgartner <Julien.Baumgartner@he-arc.ch>
2016-12-12 11:05:54 +0100

    calcul distance parcourue

--- Ressources12/BaumgartnerVaucher.py
+++ Ressources12/BaumgartnerVaucher.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
 import sys
+import math
 
 screen_x = 500
 screen_y = 500
@@ -46,6 +47,25 @@ def draw(cities):
     pygame.display.flip()
 
 
+def calculDistance(problem, index):
+    if(len(problem) != len(index)):
+        print("Les tableaux de villes et d'index ne font pas la même taille")
+    else:
+        distance = 0
+        for i in index:
+            ville1 = problem[i]
+            if(i+1 == len(problem)):
+                ville2 = problem[0]
+            else:
+                ville2 = problem[i+1]
+
+            newDistance = math.sqrt(abs(ville1.pos[0] - ville2.pos[0])*abs(ville1.pos[0] - ville2.pos[0]) + abs(ville1.pos[1] - ville2.pos[1])*abs(ville1.pos[1] - ville2.pos[1]))
+            distance += newDistance
+            print("new distance: " + str(newDistance))
+
+        print("distance parcourue: " + str(distance))
+
+
 if __name__ == '__main__':
 
     loadFile("data/pb005.txt")
@@ -72,12 +92,21 @@ if __name__ == '__main__':
                 cpt += 1
                 draw(problem)
 
+
     screen.fill(font_color)
     pygame.draw.lines(screen,city_color,True,[city.pos for city in problem])
     text = font.render("Un chemin, pas le meilleur!", True, font_color)
     textRect = text.get_rect()
     screen.blit(text, textRect)
     pygame.display.flip()
+
+
+    index = []
+    for i in range(len(problem)):
+        index.append(i)
+
+    calculDistance(problem, index)
+
 
     while True:
         event = pygame.event.wait()
