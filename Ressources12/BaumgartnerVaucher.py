f49b08fa68cccfeb9d54f9feb1c30718eb176c89
joel-vaucher <joel.vaucher@he-arc.ch>
2016-12-19 10:49:20 +0100

    fusion des solutions

--- Ressources12/BaumgartnerVaucher.py
+++ Ressources12/BaumgartnerVaucher.py
@@ -34,7 +34,37 @@ class Solution:
             yield self.problem[index]
 
     def mutation(self):
-        random.shuffle(self.indices)
+        i1 = 0
+        i2 = 0
+        while(i1 == i2):
+            i1 = randint(0,len(self.indices)-1)
+            i2 = randint(0,len(self.indices)-1)
+
+        temp = self.indices[i1]
+        self.indices[i1] = self.indices[i2]
+        self.indices[i2] = temp
+
+
+    def calculDistance(self):
+        if(len(self.problem) != len(self.indices)):
+            print("Les tableaux de villes et d'index ne font pas la même taille")
+        else:
+            distance = 0
+            for i in self.indices:
+                ville1 = self.problem[i]
+                if(i+1 == len(self.problem)):
+                    ville2 = self.problem[0]
+                else:
+                    ville2 = self.problem[i+1]
+
+                newDistance = math.sqrt(abs(ville1.pos[0] - ville2.pos[0])*
+                                        abs(ville1.pos[0] - ville2.pos[0]) +
+                                        abs(ville1.pos[1] - ville2.pos[1])*
+                                        abs(ville1.pos[1] - ville2.pos[1]))
+                distance += newDistance
+                print("new distance: " + str(newDistance))
+
+            print("distance parcourue: " + str(distance))
 
 
 problem = []
@@ -57,50 +87,8 @@ def draw(cities):
     pygame.display.flip()
 
 
-def calculDistance(problem, index):
-    if(len(problem) != len(index)):
-        print("Les tableaux de villes et d'index ne font pas la même taille")
-    else:
-        distance = 0
-        for i in index:
-            ville1 = problem[i]
-            if(i+1 == len(problem)):
-                ville2 = problem[0]
-            else:
-                ville2 = problem[i+1]
-
-            newDistance = math.sqrt(abs(ville1.pos[0] - ville2.pos[0])*abs(ville1.pos[0] - ville2.pos[0]) + abs(ville1.pos[1] - ville2.pos[1])*abs(ville1.pos[1] - ville2.pos[1]))
-            distance += newDistance
-            print("new distance: " + str(newDistance))
-
-        print("distance parcourue: " + str(distance))
-
-def mutate(index):
-
-    print("avant")
-    for i in index:
-        print(str(i))
-
-    i1 = 0
-    i2 = 0
-    while(i1 == i2):
-        i1 = randint(0,len(index)-1)
-        i2 = randint(0,len(index)-1)
 
 
-    print("i1: " + str(i1))
-    print("i2: " + str(i2))
-
-    temp = index[i1]
-    index[i1] = index[i2]
-    index[i2] = temp
-
-
-    print("après")
-    for i in index:
-        print(str(i))
-
-    return index
 
 
 if __name__ == '__main__':
@@ -132,14 +120,7 @@ if __name__ == '__main__':
     soluce = Solution(problem)
 
 
-    index = []
-    for i in range(len(problem)):
-        index.append(i)
-
-    calculDistance(problem, index)
-
-    mutate(index)
-
+    soluce.calculDistance()
 
     while True:
         soluce.mutation()
